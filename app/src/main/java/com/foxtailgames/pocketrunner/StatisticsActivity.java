140d77db75693a441329f4ded14c7dd3b8f80eac
Ricardo Delfin <ricardo.delfin@utexas.edu>
2014-12-27 22:40:27 -0600

    Added basic statistics activity
    Created a skeleton for the statistics activity. Currently it has no interactivity or information of significance. Only mock text and access from the statistics button

--- app/src/main/java/com/foxtailgames/pocketrunner/StatisticsActivity.java
+++ app/src/main/java/com/foxtailgames/pocketrunner/StatisticsActivity.java
@@ -1,26 +1,24 @@
 package com.foxtailgames.pocketrunner;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class StatisticsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_statistics);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_statistics, menu);
         return true;
     }
 
@@ -33,25 +31,9 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
-            startActivity(intent);
             return true;
-        } else if(id == R.id.action_start_run) {
-            startRun(null);
-        } else if(id == R.id.action_statistics) {
-            openStatistics(null);
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void startRun(View view) {
-        Intent intent = new Intent(getApplicationContext(), RunActivity.class);
-        startActivity(intent);
-    }
-
-    public void openStatistics(View view) {
-        Intent intent = new Intent(getApplicationContext(), StatisticsActivity.class);
-        startActivity(intent);
     }
 }
